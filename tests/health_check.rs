17c9e110ffbd638acdf8f484c7971574fdf40e9a
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-17 15:19:06 -0400

    fix typo in comment

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,7 +21,7 @@ async fn health_check_works() {
 // Only part that depends on application code
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // Retrive the port assigned to us by the OS
+    // Retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
