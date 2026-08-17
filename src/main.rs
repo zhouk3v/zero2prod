bb24d6c1314bf63ec9502f4e3a90a37660aaad36
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-17 14:19:33 -0400

    Address Clippy comment

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpRequest, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    format!("Hello {}!", name)
 }
 
 #[tokio::main]
