3caf2ffafd73085ea24ccee0efab84ae87238262
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-24 16:27:56 -0400

    Add Postgres connection

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
