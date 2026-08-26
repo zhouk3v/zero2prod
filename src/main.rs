90b9071568bfdba0eda3e2880183a6f6cedb8fa7
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-26 16:49:12 -0400

    Init env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Default to INFO logs and above if RUST_LOG is not set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
