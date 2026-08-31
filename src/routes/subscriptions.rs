b5418c14a002e63539c18a6193eb5dfd35e9c41e
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-31 13:56:24 -0400

    Add request ID to subscribe handler

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,12 +10,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber",
+        "request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber details in the database");
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -30,12 +35,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
             // Use std::fmt::Debug with `:?` to capture the error
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
