85487dd3c13232c4c2fd97ef4104e3369ec93987
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-08-31 15:34:11 -0400

    Remove info logs from subscriptions handler

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -17,16 +17,6 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     let request_span = tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name = %form.name);
 
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
@@ -42,13 +32,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             // Use std::fmt::Debug with `:?` to capture the error
             tracing::error!(
