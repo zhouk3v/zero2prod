7a9b3dd4081d96ee754c6eb04d62c2bfb6a0aad7
Kevin Zhou <k73zhou@uwaterloo.ca>
2026-09-04 14:50:44 -0400

    Use tracing logger middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
@@ -11,7 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
